5e357ec552f53f26f0e0539945bc8488d909c6d0
Santiago Varaldo <santivaraldo02@gmail.com>
2019-10-04 20:44:48 -0300

    Agrego los pines a las crocs

---
+++ SeñorDeLosAnillos/PII_RoleplayGame_Start-master/src/Library/items/Pin.cs
@@ -0,0 +1,14 @@
+namespace RoleplayGame.Items
+{
+    /// <summary>
+    /// Magia. Permite atacar y defender.
+    /// </summary>
+    public class Pin 
+    {
+
+        public override string ToString()
+        {
+            return "Pin";
+        }
+    }
+}
